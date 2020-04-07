3113dfb53b34efaf1ce96691e533e140c8ea1e49
wadihaleid <wadih.aleid@gmail.com>
2020-04-07 10:19:19 +0200

    Commit2

--- Scripts/TreesScript.R
+++ Scripts/TreesScript.R
@@ -38,6 +38,51 @@ prune.carseats = prune.misclass(tree.Carseats , best = 9)
 plot(prune.carseats)
 text(prune.carseats , pretty = 0)
 
+tree.pred = predict(prune.carseats , Carseats.test , type = "class")
+table (tree.pred , High.test)
+
+
+prune.carseats = prune.misclass(tree.Carseats , best = 15)
+plot(prune.carseats)
+text(prune.carseats, pretty = 0)
+tree.pred = predict(prune.carseats , Carseats.test , type = "class")
+table (tree.pred , High.test)
+
+
+##Fitting regression tree . Boston dataset.
+
+library(MASS)
+set.seed(1)
+train = sample(1:nrow(Boston) , nrow(Boston)/2)
+tree.boston = tree(medv~. , Boston , subset = train)
+summary(tree.boston)
+plot(tree.boston)
+text(tree.boston , pretty = 0)
+
+cv.boston = cv.tree(tree.boston)
+plot(cv.boston$size , cv.boston$dev , type = "b")
+
+prune.boston = prune.tree(tree.boston , best = 5)
+plot(prune.boston)
+text(prune.boston , pretty = 0)
+
+yhat = predict(prune.boston , newdata = Boston[-train,])
+boston.test = Boston[-train , "medv"]
+plot(yhat , boston.test)
+abline(0,1 , col = 'red')
+summary(prune.boston)
+
+## Bagging and Random forest.
+
+
+
+
+
+
+
+
+
+
 
 
 
