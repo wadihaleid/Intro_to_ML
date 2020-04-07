e904de881aeb0b287cd7e9384bd1a849a19e027c
wadihaleid <wadih.aleid@gmail.com>
2020-04-07 11:30:12 +0200

    3

--- Scripts/TreesScript.R
+++ Scripts/TreesScript.R
@@ -74,9 +74,33 @@ summary(prune.boston)
 
 ## Bagging and Random forest.
 
+library(randomForest)
+set.seed(1)
+bag.boston = randomForest(medv~. , data=Boston , subset = train , mtry = 13 , importance = TRUE)
+bag.boston
 
 
+yhat.bag = predict(bag.boston , newdata = Boston[-train , ])
+plot (yhat.bag , boston.test)
+abline (0,1 , col = 'red')
+mean ((yhat.bag - boston.test)^2)
 
+
+bag.boston = randomForest(medv~. , data=Boston , subset = train , mtry = 13 , ntree = 25)
+yhat.bag = predict(bag.boston , newdata = Boston[-train , ])
+mean ((yhat.bag - boston.test)^2)
+
+
+set.seed(1)
+rf.boston = randomForest(medv~. , data = Boston , subset = train , mtry = 6 , importance = TRUE)
+yhat.rf = predict (rf.boston , newdata = Boston[-train , ])
+mean ((yhat.rf - boston.test)^2)
+
+importance(rf.boston)
+
+varImpPlot(rf.boston)
+
+## Boosting.
 
 
 
